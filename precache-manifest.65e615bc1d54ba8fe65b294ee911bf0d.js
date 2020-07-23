8a2dca3bc10e48df1505e220d0d1c846b13a8349
Natesumo <natesumo@yahoo.com>
2020-07-23 14:13:24 -0400

    Updates

---
+++ precache-manifest.65e615bc1d54ba8fe65b294ee911bf0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4e46ee7d7962a3f6b696adcd23680e",
+    "url": "/Keto-Counter-Frontend/index.html"
+  },
+  {
+    "revision": "fb0fed9d32e076e1db25",
+    "url": "/Keto-Counter-Frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c9856a2db559e5e64804",
+    "url": "/Keto-Counter-Frontend/static/css/main.f8de3345.chunk.css"
+  },
+  {
+    "revision": "fb0fed9d32e076e1db25",
+    "url": "/Keto-Counter-Frontend/static/js/2.0c679041.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Keto-Counter-Frontend/static/js/2.0c679041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9856a2db559e5e64804",
+    "url": "/Keto-Counter-Frontend/static/js/main.b5554ad5.chunk.js"
+  },
+  {
+    "revision": "3f2935c9dd852487af64",
+    "url": "/Keto-Counter-Frontend/static/js/runtime-main.ab7d383d.js"
+  }
+]);
